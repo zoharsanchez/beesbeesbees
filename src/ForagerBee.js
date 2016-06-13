2d4617bd4ad901fde12476d55126d6ede2d44a9d
zoharsanchez <lorena.zohar@gmail.com>
2016-06-13 11:47:22 -0700

    Complete all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,12 +1,12 @@
 var ForagerBee = function() {
-	HoneyMakerBee.call(this);
+	Bee.call(this);
 	this.age = 10;
 	this.job = 'find pollen';
 	this.canFly = true;
 	this.treasureChest = [];
 };
 
-ForagerBee.prototype = new HoneyMakerBee(HoneyMakerBee.prototype);
+ForagerBee.prototype = new Bee(Bee.prototype);
 
 ForagerBee.prototype.forage = function(treasure) {
 	this.treasureChest.push(treasure);
